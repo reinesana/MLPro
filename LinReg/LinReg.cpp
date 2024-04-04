68e01895c4ffc97459d5025b63020ca717df8a80
reinesana <153279125+reinesana@users.noreply.github.com>
2024-04-03 23:53:55 -0700

    pushing cpp and header files

---
+++ LinReg/LinReg.cpp
@@ -0,0 +1,9 @@
+/* 
+ * LinReg.cpp
+ *
+ * Description: cpp file for linear regression ML algorithm
+ *
+ * Author: Shana Nursoo
+ * Date: April 3 2023
+ */
+ 
---
+++ LinReg/LinReg.h
@@ -0,0 +1,9 @@
+/* 
+ * LinReg.h
+ *
+ * Description: header file for linear regression ML algorithm
+ *
+ * Author: Shana Nursoo
+ * Date: April 3 2023
+ */
+ 
